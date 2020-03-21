80202c751f3eb4d6ed2adb913e5460c479a170d0
Ulrich Petri <ulo@ulo.pe>
2020-03-21 17:58:49 +0100

    Wire up stubs
    Connect the stubs with business logic

--- dge_ausgabeapp/blockchain.py
+++ dge_ausgabeapp/blockchain.py
@@ -1,15 +1,16 @@
 import json
 import os
+from typing import Tuple
 
 from eth_keyfile import create_keyfile_json
 from eth_typing import Address
-from web3.auto.infura import w3
+from eth_utils import to_canonical_address
 
 
-def generate_keystore(password: str) -> str:
-    return json.dumps(create_keyfile_json(os.urandom(32), password=password))
+def generate_keystore(password: bytes) -> Tuple[str, Address]:
+    keyfile_dict = create_keyfile_json(os.urandom(32), password=password)
+    return json.dumps(keyfile_dict), to_canonical_address(keyfile_dict["address"])
 
 
 def mint_tokens(address: Address, amount: int) -> None:
-    w3.eth.blockNumber
-    w3.eth.getBlock("latest")
+    ...
--- dge_ausgabeapp/cli.py
+++ dge_ausgabeapp/cli.py
@@ -1,8 +1,34 @@
+from pathlib import Path
+
 import click
+import structlog
+from structlog import get_logger
 
 from dge_ausgabeapp import __version__
+from dge_ausgabeapp.app import generate_wallet
+
+log = get_logger(__name__)
+
+
+def configure_logging() -> None:
+    structlog.get_config()["processors"].insert(0, structlog.stdlib.add_log_level)
 
 
 @click.command()
-def main() -> None:
-    click.secho(f"dgE AusgabeApp v{__version__}", fg="green")
+@click.option("-t", "--tax-id", required=True)
+@click.option("-a", "--amount", default=1000)
+@click.option(
+    "-o",
+    "--output-dir",
+    default=str(Path.cwd().absolute()),
+    show_default=True,
+    type=click.Path(dir_okay=True, file_okay=False),
+)
+def main(tax_id: str, amount: int, output_dir: str) -> None:
+    configure_logging()
+    target_path = Path(output_dir)
+
+    log.info(f"dgE AusgabeApp v{__version__}")
+
+    generate_wallet(tax_id=tax_id, amount_whole_token=amount, target_path=target_path)
+    log.info("Done")
