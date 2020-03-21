7e1d339b565926efba5941812720230984d5eed8
Vulnyl <62467691+Vulnyl@users.noreply.github.com>
2020-03-21 15:09:34 +0100

    establish connection to BC

--- dge_ausgabeapp/main.py
+++ dge_ausgabeapp/main.py
@@ -1,3 +1,11 @@
-from web3.auto.infura import w3
-w3.eth.blockNumber
-w3.eth.getBlock('latest')
+from web3 import Web3, HTTPProvider
+import json
+
+#w3.eth.blockNumber
+#w3.eth.getBlock('latest')
+
+w3 = Web3(Web3.HTTPProvider("https://"))
+from web3.middleware import geth_poa_middleware
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+w3.isConnected()
