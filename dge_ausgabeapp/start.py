fd5b663b352946eec5c58bf979a4e2284a6c63b5
Vulnyl <62467691+Vulnyl@users.noreply.github.com>
2020-03-21 15:22:40 +0100

    establish connection to BC

--- dge_ausgabeapp/start.py
+++ dge_ausgabeapp/start.py
@@ -1,4 +1,5 @@
 from web3 import Web3, HTTPProvider
+from geth import LiveGethProcess
 import json
 
 #w3.eth.blockNumber
@@ -9,3 +10,9 @@ from web3.middleware import geth_poa_middleware
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 w3.isConnected()
+
+
+#account creation
+#new_account = w3.eth.account.create('New Acc')
+geth = LiveGethProcess()
+geth.start()
