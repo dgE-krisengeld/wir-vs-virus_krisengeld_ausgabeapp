24dd737e498bdb8aa0375bdc104cbf2f82cf129e
Vulnyl <62467691+Vulnyl@users.noreply.github.com>
2020-03-21 16:52:41 +0100

    cleaned up, connection is working now as well as printing how much etherium is available in current purse

--- dge_ausgabeapp/blockchain.py
+++ dge_ausgabeapp/blockchain.py
@@ -4,6 +4,28 @@ import os
 from eth_keyfile import create_keyfile_json
 from eth_typing import Address
 from web3.auto.infura import w3
+from eth_keyfile import create_keyfile_json
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+
+input_url = "https://"
+w3 = Web3(Web3.HTTPProvider(input_url))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+
+
+def get_block() -> None:
+    w3.eth.BlockNumer
+    w3.eth.getBlock('latest')
+
+
+def print_balance() -> None:
+    balance = w3.eth.getBalance("")
+    print(w3.fromWei(balance, "ether"))  # prints etherium balance
+
+
+def print_connection_status() -> None:
+    print(w3.isConnected())  # is everything working correctly
 
 
 def generate_keystore(password: str) -> str:
