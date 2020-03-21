9a1efd6a122c5a9e66f460b14ce0686c463ddc3b
Vulnyl <62467691+Vulnyl@users.noreply.github.com>
2020-03-21 19:54:09 +0100

    Update blockchain.py

--- dge_ausgabeapp/blockchain.py
+++ dge_ausgabeapp/blockchain.py
@@ -5,6 +5,7 @@ from typing import Tuple
 from eth_keyfile import create_keyfile_json
 from eth_typing import Address
 from eth_utils import to_canonical_address
+from web3.auto.infura import w3
 
 
 def generate_keystore(password: bytes) -> Tuple[str, Address]:
@@ -14,3 +15,12 @@ def generate_keystore(password: bytes) -> Tuple[str, Address]:
 
 def mint_tokens(address: Address, amount: int) -> None:
     ...
+
+    
+def get_account_from_key(w3, key_path, passphrase):
+    with open(key_path) as key_file:
+        key_json = key_file.read()
+    private_key = w3.eth.account.decrypt(key_json, passphrase)
+    account = w3.eth.account.privateKeyToAccount(private_key)
+    return account
+
