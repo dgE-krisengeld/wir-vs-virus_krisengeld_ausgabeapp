06a84af7f33d609924e2fbad7b6bd5c41891170b
Maximilian <maxdacjs@gmail.com>
2020-03-22 17:14:53 +0100

    Fix format, lint and type issues

--- dge_ausgabeapp/blockchain.py
+++ dge_ausgabeapp/blockchain.py
@@ -1,33 +1,34 @@
 import json
 import os
 from pathlib import Path
-from typing import Tuple
+from typing import TYPE_CHECKING, Tuple
 
 from eth_keyfile import create_keyfile_json, decode_keyfile_json
-
-from structlog import get_logger
 from eth_typing import URI, Address
-from eth_utils import to_canonical_address
+from eth_utils import to_bytes, to_canonical_address
+from structlog import get_logger
 from web3 import HTTPProvider, Web3
 from web3.gas_strategies.rpc import rpc_gas_price_strategy
 
+if TYPE_CHECKING:
+    from web3.contract import ContractFunction
+
 # TODO this is görli for now
-CONTRACT_ADDRESS = Address('0x40089B4198f6adCe8F713A501cFb627a38974f79')
+CONTRACT_ADDRESS = Address(to_bytes("0x40089B4198f6adCe8F713A501cFb627a38974f79"))
 # TODO import this from the contract repo
-CONTRACT_PATH = Path(__file__).parent.absolute() / Path('../contracts/dgE.json')
+CONTRACT_PATH = Path(__file__).parent.absolute() / Path("../contracts/dgE.json")
 
 
-with open(CONTRACT_PATH, 'r') as f:
-    CONTRACT_ABI = json.load(f)['abi']
+with open(CONTRACT_PATH, "r") as f:
+    CONTRACT_ABI = json.load(f)["abi"]
 
 log = get_logger(__name__)
 
 
 def get_web3(rpc_url: str) -> Web3:
     web3 = Web3(HTTPProvider(URI(rpc_url)))
-    web3.eth.setGasPriceStrategy(rpc_gas_price_strategy)
+    web3.eth.setGasPriceStrategy(rpc_gas_price_strategy)  # type: ignore
     return web3
-
 
 
 def load_private_key(keystore_path: Path, password: bytes) -> bytes:
@@ -40,20 +41,17 @@ def generate_keystore(password: bytes) -> Tuple[str, Address]:
     return json.dumps(keyfile_dict), to_canonical_address(keyfile_dict["address"])
 
 
-def transact_function(web3, func, private_key):
-
-    nonce = web3.eth.getTransactionCount(web3.eth.defaultAccount)
+def transact_function(web3: Web3, func: "ContractFunction", private_key: bytes) -> None:
+    nonce = web3.eth.getTransactionCount(web3.eth.defaultAccount)  # type: ignore
     gas_price = web3.eth.generateGasPrice()
 
     # TODO be better informed about the estimate gas price!
     # estimate_gas = func.estimateGas()  # FIXME this doesn't work
 
     # Build a transaction that invokes this contract's function, called transfer
-    txn = func.buildTransaction({
-        'gas': 70000,
-        'gasPrice': gas_price,
-        'nonce': nonce,
-    })
+    txn = func.buildTransaction(
+        {"gas": 70000, "gasPrice": gas_price, "nonce": nonce}  # type: ignore
+    )
 
     signed_txn = web3.eth.account.sign_transaction(txn, private_key=private_key)
 
@@ -67,11 +65,8 @@ def mint_tokens(
     web3: Web3, local_private_key: bytes, target_address: Address, amount: int
 ) -> None:
 
-    log.debug(f"Contract address: {CONTRACT_ADDRESS}")
-    contract = web3.eth.contract(
-        address=CONTRACT_ADDRESS,
-        abi=CONTRACT_ABI
-     )
+    log.debug(f"Contract address: {web3.toHex(CONTRACT_ADDRESS)}")
+    contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
 
     log.info(f"Set address {web3.toChecksumAddress(target_address)} to whitelist.")
     setWhitelistAddress = contract.functions.setWhitelistAddress(target_address)
@@ -80,5 +75,3 @@ def mint_tokens(
     log.info(f"Mint {amount} dgE for address {web3.toChecksumAddress(target_address)}.")
     mintFor = contract.functions.mintFor(target_address, amount)
     transact_function(web3, mintFor, local_private_key)
-
-
