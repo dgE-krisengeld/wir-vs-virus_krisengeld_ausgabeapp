19bf3e44a912b1cfc080df76a66d49db5871cbcd
Ulrich Petri <ulo@ulo.pe>
2020-03-22 14:52:06 +0100

    Make PDFs

--- dge_ausgabeapp/app.py
+++ dge_ausgabeapp/app.py
@@ -1,6 +1,8 @@
+import webbrowser
 from datetime import datetime
 from pathlib import Path
 from typing import Callable
+from urllib.parse import quote_plus
 
 import stdnum.de.idnr
 from eth_utils import to_checksum_address
@@ -10,6 +12,8 @@ from dge_ausgabeapp.blockchain import generate_keystore, get_web3, load_private_
 from dge_ausgabeapp.output import render_paper_wallet
 
 log = get_logger(__name__)
+
+TARGET_URL_BASE = "https://krisengeld.some.domain/open?keystore="
 
 
 def tax_id_to_password(tax_id: str) -> bytes:
@@ -30,6 +34,12 @@ def tax_id_to_filename_stem(
     return f"wallet_{tax_id_formatted}_{now_callable():%Y-%m-%dT%H-%M-%S}"
 
 
+def make_url_from_keystore_json(
+    keystore_json: str, _target_url_base: str = TARGET_URL_BASE
+) -> str:
+    return f"{_target_url_base}{quote_plus(keystore_json)}"
+
+
 def generate_wallet(
     tax_id: str,
     amount_whole_token: int,
@@ -46,8 +56,8 @@ def generate_wallet(
     target_path.mkdir(exist_ok=True)
 
     generated_password = tax_id_to_password(tax_id=tax_id)
-    log.debug("Generating keystore")
     generated_keystore_json, generated_address_bin = generate_keystore(password=generated_password)
+    log.debug("Generated keystore", address=to_checksum_address(generated_address_bin))
 
     web3 = get_web3(rpc_url=rpc_url)
     local_private_key = load_private_key(keystore_path=keystore_path, password=keystore_password)
@@ -60,13 +70,15 @@ def generate_wallet(
         amount=amount_whole_token * 10 ** 18,
     )
 
-    target_file_name = Path(tax_id_to_filename_stem(tax_id=tax_id)).with_suffix(".png")
-    target_file = target_path.joinpath(target_file_name)
+    target_file_name = Path(tax_id_to_filename_stem(tax_id=tax_id)).with_suffix(".pdf")
+    target_file_path = target_path.joinpath(target_file_name)
+
+    generated_keystore_urlified = make_url_from_keystore_json(
+        keystore_json=generated_keystore_json
+    )
 
     log.info(
-        "Rendering paper wallet",
-        address=to_checksum_address(generated_address_bin),
-        target_file=str(target_file.relative_to(Path.cwd())),
+        "Rendering paper wallet", target_file=str(target_file_path.relative_to(Path.cwd())),
     )
-    print(generated_keystore_json)
-    render_paper_wallet(keystore_json=generated_keystore_json, target_file=target_file)
+    render_paper_wallet(qr_data=generated_keystore_urlified, target_file_path=target_file_path)
+    webbrowser.open(f"file://{target_file_path.absolute()}")
