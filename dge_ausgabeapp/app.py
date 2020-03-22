87a392c79ba449251e7562ef990c549153d017c9
Ulrich Petri <ulo@ulo.pe>
2020-03-22 13:32:38 +0100

    More wiring, add keystore & rpc url to CLI

--- dge_ausgabeapp/app.py
+++ dge_ausgabeapp/app.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from typing import Callable
 
 import stdnum.de.idnr
+from eth_utils import to_checksum_address
 from structlog import get_logger
 
-from dge_ausgabeapp.blockchain import generate_keystore, mint_tokens
+from dge_ausgabeapp.blockchain import generate_keystore, get_web3, load_private_key, mint_tokens
 from dge_ausgabeapp.output import render_paper_wallet
 
 log = get_logger(__name__)
@@ -26,10 +27,17 @@ def tax_id_to_filename_stem(
     tax_id: str, now_callable: Callable[[], datetime] = datetime.now
 ) -> str:
     tax_id_formatted = stdnum.de.idnr.format(tax_id).replace(" ", "-")
-    return f"wallet_{tax_id_formatted}_{now_callable():%Y-%m-%dT%H:%M:%S}"
+    return f"wallet_{tax_id_formatted}_{now_callable():%Y-%m-%dT%H-%M-%S}"
 
 
-def generate_wallet(tax_id: str, amount_whole_token: int, target_path: Path) -> None:
+def generate_wallet(
+    tax_id: str,
+    amount_whole_token: int,
+    target_path: Path,
+    rpc_url: str,
+    keystore_path: Path,
+    keystore_password: bytes,
+) -> None:
     """
 
     :raises: OSError, stdnum.exceptions.ValidationError
@@ -37,13 +45,28 @@ def generate_wallet(tax_id: str, amount_whole_token: int, target_path: Path) -> 
 
     target_path.mkdir(exist_ok=True)
 
-    password = tax_id_to_password(tax_id=tax_id)
+    generated_password = tax_id_to_password(tax_id=tax_id)
     log.debug("Generating keystore")
-    keystore_json, address_bin = generate_keystore(password=password)
+    generated_keystore_json, generated_address_bin = generate_keystore(password=generated_password)
+
+    web3 = get_web3(rpc_url=rpc_url)
+    local_private_key = load_private_key(keystore_path=keystore_path, password=keystore_password)
+
     log.info("Minting tokens", amount_whole_token=amount_whole_token)
-    mint_tokens(address=address_bin, amount=amount_whole_token * 10 ** 18)
+    mint_tokens(
+        web3=web3,
+        local_private_key=local_private_key,
+        target_address=generated_address_bin,
+        amount=amount_whole_token * 10 ** 18,
+    )
 
     target_file_name = Path(tax_id_to_filename_stem(tax_id=tax_id)).with_suffix(".png")
     target_file = target_path.joinpath(target_file_name)
-    log.info("Rendering paper wallet", target_file=str(target_file.relative_to(Path.cwd())))
-    render_paper_wallet(keystore_json=keystore_json, target_file=target_file)
+
+    log.info(
+        "Rendering paper wallet",
+        address=to_checksum_address(generated_address_bin),
+        target_file=str(target_file.relative_to(Path.cwd())),
+    )
+    print(generated_keystore_json)
+    render_paper_wallet(keystore_json=generated_keystore_json, target_file=target_file)
--- dge_ausgabeapp/cli.py
+++ dge_ausgabeapp/cli.py
@@ -24,11 +24,28 @@ def configure_logging() -> None:
     show_default=True,
     type=click.Path(dir_okay=True, file_okay=False),
 )
-def main(tax_id: str, amount: int, output_dir: str) -> None:
+@click.option(
+    "-k",
+    "--keystore-file",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    required=True,
+)
+@click.password_option("-p", "--password")
+@click.option("-r", "--rpc-url", required=True)
+def main(
+    tax_id: str, amount: int, output_dir: str, keystore_file: str, password: str, rpc_url: str
+) -> None:
     configure_logging()
     target_path = Path(output_dir)
 
     log.info(f"dgE AusgabeApp v{__version__}")
 
-    generate_wallet(tax_id=tax_id, amount_whole_token=amount, target_path=target_path)
+    generate_wallet(
+        tax_id=tax_id,
+        amount_whole_token=amount,
+        target_path=target_path,
+        rpc_url=rpc_url,
+        keystore_path=Path(keystore_file),
+        keystore_password=password.encode(),
+    )
     log.info("Done")
